69377f62ccaec06ea94c154ca59963360ace0898
rhiokim <rhio.kim@gmail.com>
2012-06-14 12:14:16 +0900

    update install

--- bin/install.sh
+++ bin/install.sh
@@ -3,3 +3,5 @@
 mkdir -p ~/Library/Scripts
 
 curl https://raw.github.com/rhiokim/Mou2Evernote/master/mou2evernote.applescript -o ~/Library/Scripts/mou2evernote.scpt
+
+echo "Enjoy Mou2Evernote AppleScript"
