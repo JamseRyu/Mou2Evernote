cde6660b5d22b27c55aa680892b578972554bbc9
rhiokim <rhio.kim@gmail.com>
2012-06-14 12:12:29 +0900

    write install.sh & Makefile

---
+++ Makefile
@@ -0,0 +1,3 @@
+init:
+	mkdir -p ~/Library/Scripts/
+	cp mou2evernote.applescript ~/Library/Scripts/mou2evernote.scpt
---
+++ bin/install.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+mkdir -p ~/Library/Scripts
+
+curl https://raw.github.com/rhiokim/Mou2Evernote/master/mou2evernote.applescript -o ~/Library/Scripts/mou2evernote.scpt
